40aa1c084c26f8789f875db8eb9b6d23898c83d1
Lamba <pslamba@gmail.com>
2014-10-25 10:17:28 -0400

    Reinitializing for Heroku

---
+++ js/server.js
@@ -0,0 +1,15 @@
+var 
+  express,
+	http,
+  port,
+	server;
+  
+express = require("express");	
+http = require("http");
+port = 80;
+server = express();
+http.createServer(server).listen(port);
+console.log("Server is listening on port " + port);
+server.get("/hello",function (req, res) {
+	res.send("Hello World!");
+});
