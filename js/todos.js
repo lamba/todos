0d65deab35301cf2542e469a46a69a50b48f4a44
Lamba <pslamba@gmail.com>
2014-10-24 21:00:48 -0400

    Added ability to remove todos

--- js/todos.js
+++ js/todos.js
@@ -1,3 +1,6 @@
+//Written by Puneet Singh Lamba
+//Inspiration by Michael Mikowski, Josh Powell, Semmy Purewal, and many others
+
 /*jslint
   browser:true,
   continue:true,
@@ -13,86 +16,141 @@
   white:true
 */
 
-//Module /main/
-//
-var main = function() {
+//Module /todos/
+var todos = function() {
   "use strict";
   
   //Module scope variables
   var
-    //Set constants
+    //Set constants		
 		url = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?",
 		tag = "&tags=nature",
-    //Declare all other module scope variables
+    
+		//Declare all other module scope variables
+		//jQuery
+		$imgElement = $("<img>"),
+		$todosList = [],
+		$todosStr,
+		$checkboxElement,
+		$labelElement,
+		$todoDiv,
+		
+		//Regular
+		todoStr,
+		idInt,
+		sequenceInt=0,
+		
+		//Module scope function names
+		addTodo,
+		printTodos,
+		removeFromTodos,
+		getPhotos;
+		
+	addTodo = function() {
+		var $newTodo;
+		if ($(".todoInput input").val() !== "") {
+			todoStr = $(".todoInput input").val();
+			
+			$todoDiv = $("<div></div>");
+			$todoDiv.attr('id',sequenceInt);
+			
+			$checkboxElement = $("<input type='checkbox'>");
+			
+			$labelElement = $("<label>");
+			$labelElement.text(" " + todoStr);
+			
+			$todoDiv.hide();
+			
+			$todoDiv.append($checkboxElement);
+			$todoDiv.append($labelElement);
+			$(".todos").append($todoDiv);
+
+			$todoDiv.fadeIn();
+			
+			$(".todoInput input").val("");
+			
+			$todosList.push($todoDiv);
+			console.log("todos array size = " + $todosList.length);
+			printTodos();
+			sequenceInt = sequenceInt + 1;
+		}
+	};
 	
-		$img = $("<img>"),
-				
-		sleep = function (milliseconds) {
-			console.log("Start sleeping");
-			var start = new Date().getTime();
-			for (var i = 0; i < 1e7; i++) {
-				if ((new Date().getTime() - start) > milliseconds){
-					break;
-				}
+	printTodos = function() {
+		$todosStr = "";
+		$todosList.forEach(function(todo_div){
+			$todosStr += todo_div.find("label").text() + ",";
+		});
+		console.log("todos:"+$todosStr);
+	};
+	
+	removeFromTodos = function(id_int) {
+		var 
+			array_index = -1,
+			remove_index = -1;
+		console.log("removing id: " + id_int);
+		$todosList.forEach(function(todo_div) {
+			array_index++;
+			if (todo_div.attr("id") === id_int) {
+				remove_index = array_index;
+				console.log("array index to remove: " + remove_index);
 			}
-			console.log("End sleeping");
-		},
-		
-		addTodo = function() {
-			var $newTodo;
-			if ($(".todoInput input").val() !== "") {
-				$newTodo = $("<p>").text($(".todoInput input").val());
-				$newTodo.hide();
-				$(".todos").append($newTodo);
-				$newTodo.fadeIn();
-				$(".todoInput input").val("");
-			}
-		},
-		
-		getPhotos = function(todo) {
-			if (todo != null) {
-				tag = "&tags=" + todo;
-			};
-			url = url + tag;
-			$.getJSON(url, function(flickrResponse) {
-				var size = flickrResponse.items.length;
-				console.log("Size = " + size);
-				console.log(JSON.stringify(flickrResponse));
-				var displayPhoto = function (photoIndex){
-					$img.hide();
-					$img.attr("src", flickrResponse.items[photoIndex].media.m);
-					$("main .photos").append($img);
-					$img.fadeIn();
-					setTimeout(function() {
-						photoIndex = photoIndex + 1;
-						if (photoIndex > size - 1) {
-							photoIndex = 0;
-							console.log("Reset index");
-						}
-						displayPhoto(photoIndex);
-					}, 2000);
-				};
-				displayPhoto(0);		
-				$(".todoInput input").focus();
-			});
+		});
+		$todosList.splice(remove_index,1);
+	};
+	
+	getPhotos = function(todo) {
+		if (todo != null) {
+			tag = "&tags=" + todo;
 		};
+		url = url + tag;
+		$.getJSON(url, function(flickrResponse) {
+			var size = flickrResponse.items.length;
+			console.log("Size = " + size);
+			console.log(JSON.stringify(flickrResponse));
+			var display_photo = function (photo_index){
+				$imgElement.hide();
+				$imgElement.attr("src", flickrResponse.items[photo_index].media.m);
+				$("main .photos").append($imgElement);
+				$imgElement.fadeIn();
+				setTimeout(function() {
+					photo_index = photo_index + 1;
+					if (photo_index > size - 1) {
+						photo_index = 0;
+						console.log("Reset index");
+					}
+					display_photo(photo_index);
+				}, 2000);
+			};
+			display_photo(0);		
+			$(".todoInput input").focus();
+		});
+	};
 		
-	console.log("DOM is ready!");
 	getPhotos();
 	
+	//Event handlers
   $(".todoInput button").on("click", function(event){
 		addTodo();
 		$(".todoInput input").focus();
   });
   
   $(".todoInput input").on("keypress", function(event){
-	if (event.keyCode === 13) {
-	  addTodo();
+		if (event.keyCode === 13) {
+			addTodo();
+			$(".todoInput input").focus();
+		}
+	});
+  
+  $(".todos").on('change', 'input[type=checkbox]', function(event){
+		idInt = $(event.target).parent().attr("id");
+		$(event.target).parent().fadeOut().remove();
+		removeFromTodos(idInt);
+		printTodos();		
 		$(".todoInput input").focus();
-	}
-  });
+	});
   
 };	
   
 //Start app once DOM is ready
-$(document).ready(main);
+$(todos);
