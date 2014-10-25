b9626af4f52da794fd491fa4a66f77060eec93ab
Lamba <pslamba@gmail.com>
2014-10-25 13:01:56 -0400

    Changed port back to 80 and added env port

--- js/server.js
+++ js/server.js
@@ -6,10 +6,10 @@ var
   
 express = require("express");	
 http = require("http");
-port = 3000;
+port = 80;
 
 server = express();
-http.createServer(server).listen(port);
+http.createServer(server).listen(process.env.PORT || port);
 console.log("Server is listening on port " + port);
 server.get("/",function (req, res) {
 	res.send("Hello World!");
